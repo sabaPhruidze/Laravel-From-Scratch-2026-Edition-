597b391d7c10c8c9f04b00b011cced94dcc9dcf1
Saba <sabaphruidze01@gmail.com>
2026-06-05 12:15:14 +0400

    feat(components):learned how to create common file that will be same for others as well and how to integrate child file using slot

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/about',function() {
-    return view('about');
-});
-Route::get('/contact',function() {
-    return view('contact');
-});
+// shortly written 
+Route::view('/about','about');
+Route::view('/contact','contact');
