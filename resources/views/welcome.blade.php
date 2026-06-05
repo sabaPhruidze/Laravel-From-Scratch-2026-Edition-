eb1a73076e383ea24232b7a43f7272fd6db0b323
Saba <sabaphruidze01@gmail.com>
2026-06-05 11:53:20 +0400

    feat(routes): in web file added one more route with the return of the string

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,9 +17,9 @@
             </style>
         @endif
     </head>
-    <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+    <body class="bg-white">
         <div>
-            hello word
+            <a href="about">About us</a>
         </div>
     </body>
 </html>
