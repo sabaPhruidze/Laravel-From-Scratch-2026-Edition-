d0ac1ecfd9ef56a22e5a05afdbd333c246daf14c
Saba <sabaphruidze01@gmail.com>
2026-06-08 13:02:52 +0400

    feat(routes):learned how to use PATCH method

---
+++ resources/views/ideas/edit.blade.php
@@ -0,0 +1,25 @@
+<x-layout>
+    <form method='POST' action="/ideas/index/{{ $idea->id }}">
+        {{-- method spoofing --}}
+        @csrf
+        @method('PATCH');
+        {{-- დაემატება hidden input --}}
+        <div class="col-span-full">
+            <label for="description" class="block text-sm/6 font-medium text-white">edit your idea</label>
+            <div class="mt-2">
+                {{-- dd(request()->all()); ეს წამოიღებს ტოკენს და ასევე ტესტს რაც ჩვწერეთ textareaში --}}
+                <textarea id="description" name="description" rows="3"
+                    class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6 border-3 border-amber-400">
+                {{ $idea->description }}
+                </textarea>
+            </div>
+
+        </div>
+        <div class="mt-6 flex items-center gap-x-6">
+            <button type="submit"
+                class="cursor-pointer rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Update</button>
+
+        </div>
+    </form>
+
+</x-layout>
--- resources/views/ideas/index.blade.php
+++ resources/views/ideas/index.blade.php
@@ -8,10 +8,10 @@
         თუ ემთხვევა — request გადის , თუ არ ემთხვევა — Laravel აგდებს 419 Page Expired შეცდომას.
         --}}
         <div class="col-span-full">
-            <label for="about" class="block text-sm/6 font-medium text-white">New ideas</label>
+            <label for="description" class="block text-sm/6 font-medium text-white">New ideas</label>
             <div class="mt-2">
                 {{-- dd(request()->all()); ეს წამოიღებს ტოკენს და ასევე ტესტს რაც ჩვწერეთ textareaში --}}
-                <textarea id="about" name="about" rows="3"
+                <textarea id="description" name="description" rows="3"
                     class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6 border-3 border-amber-400"></textarea>
             </div>
             <p class="mt-3 text-sm/6 text-gray-400">Have an idea you want to save for later?</p>
