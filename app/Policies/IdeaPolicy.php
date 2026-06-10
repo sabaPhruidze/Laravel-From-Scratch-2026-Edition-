23fb080c2409fc6e6aa7022273f35a28342276f3
Saba <sabaphruidze01@gmail.com>
2026-06-10 12:43:10 +0400

    feat(IdeaPolicy):written couple of versions how to write same check

--- app/Policies/IdeaPolicy.php
+++ app/Policies/IdeaPolicy.php
@@ -14,6 +14,8 @@ class IdeaPolicy
     public function update(User $user, Idea $idea): bool
     {
         //return false;  // ამ მომენტში არცერთის update საშუალებას არ მაძლევს 403ით
-        return $user->id === $idea->user_id; //ვამოწმებ და თუ შეესაბამება user id idea_ს user_id მაშინ მისცეს უფლებას
+        //return $user->id === $idea->user_id; //ვამოწმებ და თუ შეესაბამება user id idea_ს user_id მაშინ მისცეს უფლებას
+        //return $user->id === $idea->user_id ? Response::allow() : Response::denyAsNotFound(); // თუ სხვისი იდეის ნახვას ცდილობს 404 ერორს დაუწერს
+        return $user->is($idea->user); // ესეც იგივეა Determine if two models have the same ID and belong to the same table.
     }
 }
