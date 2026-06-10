ac086fbd541dd3c13e7a12bfa48734093004ed15
Saba <sabaphruidze01@gmail.com>
2026-06-10 09:53:35 +0400

    comments(boostrap): in app folder learned more what it consists

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,7 @@
 <?php
 // აქ იქმნება Laravel-ის მთავარი application instance.
 //ეს application იყენებს ძალიან მნიშვნელოვან ნაწილს:
-//Service Container
-//Service Container არის Laravel-ის „საწყობი“
-//ან „მართვის ცენტრი“. მარტივად რომ ვთქვათ:
+//Service Container არის Laravel-ის „საწყობი“ ან „მართვის ცენტრი“.
 //Laravel აქ ინახავს და მართავს საჭირო ობიექტებს/სერვისებს.
 
 use Illuminate\Foundation\Application;
@@ -11,15 +9,18 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 
+//Application::configure იწყებს Laravel-ის მთავარი Application-ის კონფიგურაციას.
 return Application::configure(basePath: dirname(__DIR__))
+// route ფაილები აქ არის
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
+    // როცა middleware დაგჭირდება, ეს წესები გამოიყენე
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->redirectGuestsTo('login');
-        // ამით როცა middleware ით დაცულ გვერდზე შევ მომხმარებელი
+        // ამით როცა middleware ით დაცულ გვერდზე შევა მომხმარებელი
         //თუ ავტორიზებული არ იქნება login გვერდზე გაუშვებს
         $middleware->redirectUsersTo('/ideas/index');
         //ეს დალოგინებუს გადაამისამართებს /ideas/index ზე თუ ეცდება აკრძალულ
@@ -27,7 +28,8 @@ return Application::configure(basePath: dirname(__DIR__))
 
     })
     ->withExceptions(function (Exceptions $exceptions): void {
+        // თუ error/exception მოხდება, როგორ მოიქცე
         $exceptions->shouldRenderJsonWhen(
             fn(Request $request) => $request->is('api/*'),
         );
-    })->create();
+    })->create(); //აქ უკვე იქმნება საბოლოო Application Instance.
